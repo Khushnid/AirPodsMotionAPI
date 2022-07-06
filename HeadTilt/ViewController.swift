a47de3e9e7212e86b2730eaedfcc94fa18b8667f
Khushnid <99318942+chowiskhush@users.noreply.github.com>
2022-07-06 14:15:46 +0500

    Refactor code

--- HeadTilt/ViewController.swift
+++ HeadTilt/ViewController.swift
@@ -2,49 +2,76 @@ import UIKit
 import CoreMotion
 
 class ViewController: UIViewController {
-   
+    
+    let SPEED_MULTIPLIER: Double = 12
     let manager = CMHeadphoneMotionManager()
     
-    @IBOutlet weak var cursorImage: UIImageView!
-    let SPEED_MULTIPLIER: Double = 12
-  
+    let cursorImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "cursor")
+        return image
+    }()
+    
+    let statusTitle: UILabel = {
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.font = UIFont.boldSystemFont(ofSize: 16)
+        title.textColor = .black
+        return title
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         manager.delegate = self
+        setupConstraints()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         setupAirPods()
     }
-}
-
-extension ViewController {
+    
+    private func setupConstraints() {
+        view.addSubview(statusTitle)
+        view.addSubview(cursorImage)
+        
+        NSLayoutConstraint.activate([
+            statusTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            statusTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         
+            cursorImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cursorImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            cursorImage.heightAnchor.constraint(equalToConstant: 20),
+            cursorImage.widthAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
     private func setupAirPods() {
-        manager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: { [self] (deviceMotion, error) -> Void in
+        manager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: { [self] deviceMotion, _ -> Void in
             if let motion = deviceMotion {
                 let pitch = degrees(motion.attitude.pitch)
                 let yaw = degrees(motion.attitude.yaw)
-                cursorImage.center = CGPoint(x: view.center.x - yaw * SPEED_MULTIPLIER, y: view.center.y - pitch * SPEED_MULTIPLIER)
-            } else {
-                print("ERROR: \(error?.localizedDescription ?? "")")
+                let xCoordinate = view.center.x - yaw * SPEED_MULTIPLIER
+                let yCoordinate = view.center.y - pitch * SPEED_MULTIPLIER + UIScreen.main.bounds.width / 3
+                cursorImage.center = CGPoint(x: xCoordinate, y: yCoordinate)
             }
         })
     }
-}
-
-extension ViewController {
+    
     func degrees(_ radians: Double) -> Double {
         return 180 / .pi * radians
     }
 }
 
 extension ViewController: CMHeadphoneMotionManagerDelegate {
-    func headphoneMotionManagerDidConnect(_ manager: CMHeadphoneMotionManager) {
-        print("AirPods Connected!")
+   func headphoneMotionManagerDidConnect(_ manager: CMHeadphoneMotionManager) {
+        view.backgroundColor = .white
+        statusTitle.text = "Connected"
     }
     
     func headphoneMotionManagerDidDisconnect(_ manager: CMHeadphoneMotionManager) {
-        print("AirPods Disconnected!")
+        view.backgroundColor = .red.withAlphaComponent(0.6)
+        statusTitle.text = "Disconnected"
     }
 }
